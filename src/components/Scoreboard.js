fa011006da721fe643c6fddd6350c859c1aac2c6
Iivari Anttila <iivari03@gmail.com>
2025-04-18 00:06:00 +0300

    Update scoreboard filler text

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const Scoreboard = () => {
+    const { game } = useContext(GlobalContext);
+
+
     return (
         <h3>
             The Scoreboard would go here!!!
+            <p>Player amount: {game.players}</p>
+            <p>Round amount: {game.rounds}</p>
         </h3>
     )
 }
