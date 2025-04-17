ae667eb62222607da55ed9f4122db46a23b96c0b
Iivari Anttila <iivari03@gmail.com>
2025-04-17 22:37:10 +0300

    Created header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import { Header } from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is some temporary text</h1>
+    <div>
+      <Header />
     </div>
   );
 }
